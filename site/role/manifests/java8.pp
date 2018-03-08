4cceaeade5f9faef8a1abca059f3eda6ca44e55a
renatoadsumus <renatoadsumus@gmail.com>
2018-03-08 16:02:10 -0300

    Add file java8.pp

---
+++ site/role/manifests/java8.pp
@@ -0,0 +1,11 @@
+class java8 {
+	
+	package{'java-1.8.0-openjdk-devel':
+		ensure => present,
+	} ->		
+	file_line { "/etc/environment":	
+		path => "/etc/environment",  			
+		line => "JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk",
+		match => "^JAVA_HOME.*$",
+	}
+}
