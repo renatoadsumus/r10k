e02c7e5e4fcfea2cbb061d0a8bf216bd445f576b
renatoadsumus <renatoadsumus@gmail.com>
2018-04-13 13:15:44 -0300

    Alter name of Node

--- site/profile/manifests/docker.pp
+++ site/profile/manifests/docker.pp
@@ -2,9 +2,7 @@ class profile::docker inherits profile::base{
 	
   ### CRIANDO A PASTA /OPT/AGENTS/GO-AGENT
 	file{"/opt/site-conf-r10k/":
-		ensure  => "directory",
-		owner   => "tfsservice",
-		group   => "suporte",		
+		ensure  => "directory",			
 	}
 	
 }
