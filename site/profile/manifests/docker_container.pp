eb7c2c755e974fccac79caf52be8676950045a9e
renatoadsumus <renatoadsumus@gmail.com>
2018-04-19 14:30:46 -0300

    Using run docker module

--- site/profile/manifests/docker_container.pp
+++ site/profile/manifests/docker_container.pp
@@ -1,11 +1,19 @@
 class profile::docker_container inherits profile::base{	
 
-	#include ::docker
+	include ::docker
 	
+	# fetch the docker image
 	::docker::image { 'nginx':	
 		ensure    => 'present',
 		image_tag => 'stable-alpine',
 		require   => Class['docker'],
 	}  
+	
+	# run the container using the image above
+  ::docker::run { 'nginx':
+		image   => 'nginx:stable-alpine',
+		ports   => ['8080:80'],
+		require => Docker::Image['nginx'],
+  }
   
 }
