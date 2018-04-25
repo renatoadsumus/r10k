b68d1e89792ae8aba25f0010e42156e9047203c7
renatoadsumus <renatoadsumus@gmail.com>
2018-04-25 12:29:27 -0300

    Refactory docker add user docker group and run go-server docker

--- site/profile/manifests/docker_container.pp
+++ site/profile/manifests/docker_container.pp
@@ -12,7 +12,19 @@ class profile::docker_container inherits profile::base{
   #}
   
   exec {'gocd_server':
-        command  => 'docker run -d -p 8080:8153 renatoadsumus/gocd:latest',
-        user => 'root',   		
+        command  => 'docker run --rm -d -p 8089:8153 --name gocd_server --dns=172.17.32.98 --dns-search=ogmaster.local renatoadsumus/gocd:latest',       	
+		path => ['/usr/bin',],
+		onlyif  => 'test -e /etc/docker/container_gocd_server_execucao.txt',
     }
+	
+	file {'container_gocd_server_execucao':
+        ensure => 'file',
+        path => '/etc/docker/container_gocd_server_execucao.txt',
+        content => 'Container com GO SERVER em execucao',  
+		require => [Exec['gocd_server']],
+	}
+	
 }
+
+
+
--- site/profile/manifests/install_docker.pp
+++ site/profile/manifests/install_docker.pp
@@ -1,24 +1,4 @@
 class profile::install_docker{
-
-	#include ::docker
-
-	#class { 'docker' :
-		#use_upstream_package_source => false,
-		#service_overrides_template  => false,
-		#docker_ce_package_name  => 'docker',
-	#  
-
-	#class { 'docker':
-			#docker_ee => true,
-			#docker_ee_source_location => 'https://download.docker.com/linux/centos/docker-ce.repo',
-			#'https://<docker_ee_repo_url>',
-			#docker_ee_key_source => 'https://<docker_ee_key_source_url>',
-	#}
-
-	 #baseurl  => "https://download.docker.com/linux/centos/7/\$basearch/stable",
-        #proxy    => "$proxy",
-        #gpgcheck => 1,
-        #gpgkey   => "https://download.docker.com/linux/centos/gpg",
 		
 	service { 'docker':
         ensure  => running,
@@ -39,6 +19,13 @@ class profile::install_docker{
         path => '/etc/docker/daemon.json',
         content => '{"bip":"10.66.33.10/24"}',  
 		require => [Package['docker']],
+	}
+	
+	exec {'add_user_docker_groupo':
+        command  => 'usermod -aG docker ec2-user',   
+		path => ['/usr/bin'],
+		require => [File['daemon.json']],
+		onlyif  => 'test ! -e /etc/docker/daemon.json',
     }
 	
 }
