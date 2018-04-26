826725506ae0ebe3906fdeb7624248b52d621e46
renatoadsumus <renatoadsumus@gmail.com>
2018-04-26 11:08:49 -0300

    Incluind test to run container go-server

--- site/profile/manifests/docker_container.pp
+++ site/profile/manifests/docker_container.pp
@@ -1,20 +1,9 @@
-class profile::docker_container inherits profile::base{		
-	
-	#::docker::image { 'renatoadsumus/gocd':	
-		#ensure    => 'present',
-		#image_tag => 'latest',		
-	#}  	
-	
-	#::docker::run { 'gocd_server':
-		#image   => 'renatoadsumus/gocd:latest',
-		#ports   => ['8080:8153'],
-		#require => Docker::Image['renatoadsumus/gocd'],
-  #}
+class profile::docker_container inherits profile::base{	
   
   exec {'gocd_server':
         command  => 'docker run --rm -d -p 8089:8153 --name gocd_server --dns=172.17.32.98 --dns-search=ogmaster.local renatoadsumus/gocd:latest',       	
 		path => ['/usr/bin',],
-		onlyif  => 'test -e /etc/docker/container_gocd_server_execucao.txt',
+		onlyif  => 'test ! -e /etc/docker/container_gocd_server_execucao.txt',
     }
 	
 	file {'container_gocd_server_execucao':
@@ -23,7 +12,7 @@ class profile::docker_container inherits profile::base{
         content => 'Container com GO SERVER em execucao',  
 		require => [Exec['gocd_server']],
 	}
-	
+		
 }
 
 
---
+++ site/profile/manifests/docker_container_onpremisse.pp
@@ -0,0 +1,15 @@
+class profile::docker_container inherits profile::base{		
+	
+	::docker::image { 'renatoadsumus/gocd':	
+		ensure    => 'present',
+		image_tag => 'latest',		
+	}  	
+	
+	::docker::run { 'gocd_server':
+		image   => 'renatoadsumus/gocd:latest',
+		ports   => ['8080:8153'],
+		require => Docker::Image['renatoadsumus/gocd'],
+  }
+  
+ 
+}
